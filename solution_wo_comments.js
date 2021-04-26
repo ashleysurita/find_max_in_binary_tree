be0fce166795f779796f1658c8082043e75c46be
Ashley <ashleywong678@gmail.com>
2021-04-26 09:35:15 -0400

    Update solution_wo_comments.js

--- solution_wo_comments.js
+++ solution_wo_comments.js
@@ -7,23 +7,15 @@ class TreeNode {
 }
 
 function findMaxBT(root) {
-    if(!root) return null    
+    if(!root) return null
+    
     const left = root.left ? findMaxBT(root.left) : null
     const right = root.right ? findMaxBT(root.right) : null
-    let biggest = root.value    
-    if(left && left > biggest) biggest = left
-    if(right && right > biggest) biggest = right    
-    return biggest
+    return Math.max(root.value, left, right)
 }
 
 // Test Cases
 console.log(findMaxBT(null)) // null
 console.log(findMaxBT(new TreeNode(1, new TreeNode(2), new TreeNode(3)))) // 3
-console.log(findMaxBT(new TreeNode(2, 
-                                   new TreeNode(29, 
-                                                new TreeNode(26)),
-                                   new TreeNode(4, 
-                                                null, 
-                                                new TreeNode(2, 
-                                                             new TreeNode(9)))))) // 29
+console.log(findMaxBT(new TreeNode(2, new TreeNode(29, new TreeNode(26)), new TreeNode(4, null, new TreeNode(2, new TreeNode(9)))))) // 29
 console.log(findMaxBT(new TreeNode(1))) // 1
